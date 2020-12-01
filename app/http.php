f97e167b51b6f7e2b7a0b24aabe7f76ec5762462
Omri Shapira <omri@redefinemeat.com>
2020-12-01 14:33:09 +0200

    udpdate

--- app/http.php
+++ app/http.php
@@ -3,87 +3,14 @@
 if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require __DIR__.'/../vendor/autoload.php';
 }
-use Utopia\Swoole\Files;
+
+use Utopia\App;
 use Utopia\Swoole\Request;
 use Utopia\Swoole\Response;
-use Swoole\Process;
+use Utopia\Swoole\Files;
 use Swoole\Http\Server;
 use Swoole\Http\Request as SwooleRequest;
 use Swoole\Http\Response as SwooleResponse;
-use Utopia\App;
-
-class MessageEntity {
-    public $id;
-    public $message;
-}
-
-$fakeData = array();
-
-App::get('/messages')
-     // Define Route
-    ->action(
-        function() use ($request, $response) {
-            $response
-              ->json([]);
-        }, ['response']);
-
-App::post('/messages')
-    ->param('message', '', 'Message from user')
-    ->param('id', '', 'Id of the message')
-    ->action(
-        function() use($message, $id, $response) {
-            $entity = new MessageEntity();
-            $entity->$id = $id;
-            $entity->$message = $message;
-
-            for ($i = 0; $i < $length; $i++) {
-                if ($fakeData[$i]->$id == $id) {
-                    throw new Exception('Message is already exist with this Id.', 409);
-                }
-            }
-
-            array_push($fakeData, $entity);
-            $response
-                ->setStatusCode(Response::STATUS_CODE_CREATED)
-                ->json($entity);
-        }, ['response']);
-
-App::delete('/messages/:id')
-    ->param('id', '', 'Id of the message')
-    ->action(
-        function() use($id, $response) {
-            $length = count($fakeData);
-            for ($i = 0; $i < $length; $i++) {
-                if ($fakeData[$i]->$id == $id) {
-                    unset($fakeData[$i]);
-                }
-            }
-
-            array_values($fakeData);
-
-            $response
-                ->setStatusCode(Response::STATUS_CODE_OK);
-        }, ['response']);
-
-App::put('/messages/:id')
-    ->param('message', '', 'Message from user')
-    ->param('id', '', 'Id of the message')
-    ->action(
-        function() use($message, $id, $response) {
-
-            for ($i = 0; $i < $length; $i++) {
-                if ($fakeData[$i]->$id == $id) {
-                    $fakeData[$i]->$message = $message;
-                }
-            }
-
-            $response
-                ->setStatusCode(Response::STATUS_CODE_OK)
-                ->json([
-                    'message' => $message,
-                    'id' => $id
-                ]);
-        }, ['response']);
 
 $http = new Server("0.0.0.0", 80);
 
---
+++ app/messages.php
@@ -0,0 +1,85 @@
+<?php
+
+use Utopia\App;
+use Utopia\Exception;
+
+class MessageEntity {
+    public $id;
+    public $message;
+}
+
+$fakeData = array();
+
+App::get('/messages')
+    ->desc('Get All messages')
+    ->groups(['api', 'messages'])
+     // Define Route
+    ->action(
+        function() use ($request, $response) {
+            $response
+              ->json([]);
+        }, ['response']);
+
+App::post('/messages')
+    ->desc('Add new message')
+    ->groups(['api', 'messages'])
+    ->param('message', '', 'Message from user')
+    ->param('id', '', 'Id of the message')
+    ->action(
+        function() use($message, $id, $response) {
+            $entity = new MessageEntity();
+            $entity->$id = $id;
+            $entity->$message = $message;
+
+            for ($i = 0; $i < $length; $i++) {
+                if ($fakeData[$i]->$id == $id) {
+                    throw new Exception('Message is already exist with this Id.', 409);
+                }
+            }
+
+            array_push($fakeData, $entity);
+            $response
+                ->setStatusCode(Response::STATUS_CODE_CREATED)
+                ->json($entity);
+        }, ['response']);
+
+App::delete('/messages/:id')
+    ->desc('Delete message by Id')
+    ->groups(['api', 'messages'])
+    ->param('id', '', 'Id of the message')
+    ->action(
+        function() use($id, $response) {
+            $length = count($fakeData);
+            for ($i = 0; $i < $length; $i++) {
+                if ($fakeData[$i]->$id == $id) {
+                    unset($fakeData[$i]);
+                }
+            }
+
+            array_values($fakeData);
+
+            $response
+                ->setStatusCode(Response::STATUS_CODE_OK);
+        }, ['response']);
+
+App::put('/messages/:id')
+    ->desc('Edit message by Id')
+    ->groups(['api', 'messages'])
+    ->param('message', '', 'Message from user')
+    ->param('id', '', 'Id of the message')
+    ->action(
+        function() use($message, $id, $response) {
+
+            for ($i = 0; $i < $length; $i++) {
+                if ($fakeData[$i]->$id == $id) {
+                    $fakeData[$i]->$message = $message;
+                }
+            }
+
+            $response
+                ->setStatusCode(Response::STATUS_CODE_OK)
+                ->json([
+                    'message' => $message,
+                    'id' => $id
+                ]);
+        }, ['response']);
